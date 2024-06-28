39483f71745597886cc1bd756ce5a8f636acdecc
David Paul McQuiggin <david@pro-coded.com>
2024-06-28 01:25:46 +0100

    Initial commit

---
+++ Builder/Program.cs
@@ -0,0 +1,26 @@
+﻿using Builder;
+using Builder.HouseBuilder;
+
+var director = new HouseDirector();
+IHouseBuilder luxuryBuilder = new LuxuryHouseBuilder();
+IHouseBuilder ecoFriendlyBuilder = new EcoFriendlyHouseBuilder();
+
+List<string> luxuryAmenities = new List<string> { "Swimming pool", "Home theater" };
+List<string> ecoFriendlyAmenities = new List<string> { "Energy-efficient appliances" };
+
+// Construct a luxury house
+director.Construct(luxuryBuilder, 5, true, luxuryAmenities);
+var luxuryHouse = luxuryBuilder.GetHouse();
+
+// Construct an eco-friendly house
+director.Construct(ecoFriendlyBuilder, 3, false, ecoFriendlyAmenities);
+var ecoFriendlyHouse = ecoFriendlyBuilder.GetHouse();
+
+// Display house details
+Console.WriteLine("Luxury House:");
+luxuryHouse.Display();
+
+Console.WriteLine("\nEco-Friendly House:");
+ecoFriendlyHouse.Display();
+
+Console.ReadLine();
